6512c9738c5d22ae79c94a3873b9e1bf062873b7
Hisham Bin Seddeq <prog.hbs@hotmail.com>
2019-09-19 19:45:08 +0300

    created the cards object

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,11 +1,24 @@
-// var cardOne = "king";
-// var cardTwo = "king";
-// var cardThree = "queen";
-// var cardFour = "queen";
-
-
-
-var cards = ["king", "king", "queen", "queen"];
+var cards = [{
+        rank: "queen",
+        suit: "hearts",
+        cardImage: "images/queen-of-hearts.png"
+    },
+    {
+        rank: "queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png"
+    },
+    {
+        rank: "king",
+        suit: "hearts",
+        cardImage: "images/king-of-hearts.png"
+    },
+    {
+        rank: "king",
+        suit: "diamonds",
+        cardImage: "images/king-of-diamonds.png"
+    }
+];
 var cardsInPlay = [];
 
 function checkForMatch() {
@@ -22,8 +35,10 @@ function checkForMatch() {
 
 function flipCard(cardId) {
 
-    console.log("user flipped " + cards[cardId])
-    cardsInPlay.push(cards[cardId]);
+    console.log("user flipped " + cards[cardId].rank);
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
+    cardsInPlay.push(cards[cardId].rank);
     checkForMatch();
 
 }
